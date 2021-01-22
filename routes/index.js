36a74bba430f7db99ba07153319345cf2e689734
yifulin233 <2327397490@qq.com>
2021-01-22 15:43:43 +0800

    这是爱你爱上覅按

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+//登录路由配置
+router.get('/login', function (req, res) {
+  res.render('login', {});
+});
+
+//文章详情页路由配置
+router.get('/article', function (req, res) {
+  res.render('details', {});
+});
+
+
 
 module.exports = router;
