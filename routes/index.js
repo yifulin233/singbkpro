07d836ed9e163cb7a1eaf06241f56c9191fe7e9d
yifulin233 <2327397490@qq.com>
2021-01-27 19:20:36 +0800

    详情页渲染、编辑跳转实现

--- routes/index.js
+++ routes/index.js
@@ -26,6 +26,7 @@ router.get('/', async function (req, res, next) {
   // 确定每页显示的数据
   data.blogList = await Article.find()
     .limit(pageSize)//限定展示出来的条数
+    .sort({ _id: 'desc' })//倒叙
     .skip((data.currPage - 1) * pageSize)//限定从第几条开始截取
   // 总数据
   let blogAll = await Article.find()
@@ -34,10 +35,10 @@ router.get('/', async function (req, res, next) {
   // console.log(data.PagesTotle);
   // 将所有的时间戳转换成时间
   data.blogList.map(item => {
-    // let a = moment(item.id).format('MMM Do YYYY')
-    // let a = moment(item.id).format('LLLL');
-    let a = moment(item.id).format('MMMM Do YYYY, h:mm:ss a');
+    // let a = moment(item.id).format('MMMM Do YYYY, h:mm:ss a');
+    let a = moment(item.time).utcOffset(480).format('YYYY-MM-DD HH:mm:ss');
     item['time'] = a
+    // console.log(a)
   })
 
   res.render('index', { userName, data });
@@ -49,16 +50,37 @@ router.get('/login', function (req, res) {
 });
 
 //dateils详情页路由配置
-router.get('/details', function (req, res) {
-  let userName = req.session.userName || ''
-  res.render('details', { userName });
+router.get('/details', async function (req, res) {
+  let userName = req.session.username || ''
+
+  let blokId = req.query._id
+  console.log('哦哦'+blokId);
+  let data = await Article.findOne({ _id: blokId })
+  data['time'] = moment(data.id).utcOffset(480).format('YYYY-MM-DD HH:mm:ss');
+  res.render('details', { userName, data });
 });
 
 
 //write路由配置
-router.get('/write', function (req, res) {
+router.get('/write', async (req, res, next) => {
   let userName = req.session.userName || ''
-  res.render('write', { userName });
+
+  let _id = req.query._id || ''
+  if (_id) {
+    let page = req.query.page
+
+    console.log('这是1'+_id);
+    console.log('这是2'+page);
+
+    // 文章数据查询渲染
+
+    let details = await Article.findOne({ _id: _id })
+    // 时间处理
+    res.render('write', { userName, details });
+
+  } else {
+    res.render('write', { userName });
+  }
 });
 
 //regist路由配置
