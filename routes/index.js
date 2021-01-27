17b248a369c22cdb60e109012874a3dc7af5d4cc
yifulin233 <2327397490@qq.com>
2021-01-27 14:19:52 +0800

    初步首页渲染

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,42 @@ var router = express.Router();
 // 文章模板导入
 let Article = require('../models/article')
 
+let moment = require('moment')
 
 
 /* GET home page. */
 router.get('/', async function (req, res, next) {
-  let data = await Article.find()
-  console.log(data);
+  let cPage = req.query.page || 1
+  // console.log(cPage);
+  // let data = await Article.find()
+  // console.log(data);
   let userName = req.session.username || ''
+
+  let data = {
+    blogList: [],//文章列表
+    currPage: cPage,//当前页数
+    PagesTotle: '',//总页数
+  }
+
+  // 设定每页渲染的条数
+  let pageSize = 4
+  // 确定每页显示的数据
+  data.blogList = await Article.find()
+    .limit(pageSize)//限定展示出来的条数
+    .skip((data.currPage - 1) * pageSize)//限定从第几条开始截取
+  // 总数据
+  let blogAll = await Article.find()
+  // 总页码
+  data.PagesTotle = Math.ceil(blogAll.length / pageSize)
+  // console.log(data.PagesTotle);
+  // 将所有的时间戳转换成时间
+  data.blogList.map(item => {
+    // let a = moment(item.id).format('MMM Do YYYY')
+    // let a = moment(item.id).format('LLLL');
+    let a = moment(item.id).format('MMMM Do YYYY, h:mm:ss a');
+    item['time'] = a
+  })
+
   res.render('index', { userName, data });
 });
 
